4e28a972481e49ecbef8a982905e1df8a81a8cf7
zhangzihao <zhangzihao@17paipai.cn>
2026-06-04 13:40:43 +0800

    Hot Reload 提交

---
+++ Assets/HotReload/testReload.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testReload : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("1112211111");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,1,0);
+        
+    }
+}
